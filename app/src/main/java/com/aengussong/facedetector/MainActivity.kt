361815c99e9ae6ec6fdc80d611e4d359317dbcba
coolsmileman@gmail.com <oleg.d@spiralsoft@co.il>
2020-03-22 23:25:29 +0200

    first screen logic implementation
    Added calls to start Camera and List Activity on button press.

--- app/src/main/java/com/aengussong/facedetector/MainActivity.kt
+++ app/src/main/java/com/aengussong/facedetector/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.aengussong.facedetector
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_start.setOnClickListener { startActivity(Intent(this, CameraActivity::class.java)) }
+        main_list.setOnClickListener { startActivity(Intent(this, ListActivity::class.java)) }
     }
 }
