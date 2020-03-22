74af96ee90365e447ad34bde53d239b31327693b
coolsmileman@gmail.com <oleg.d@spiralsoft@co.il>
2020-03-22 23:43:51 +0200

    add openCV

--- app/src/main/java/com/aengussong/facedetector/CameraActivity.kt
+++ app/src/main/java/com/aengussong/facedetector/CameraActivity.kt
@@ -2,6 +2,8 @@ package com.aengussong.facedetector
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import org.opencv.android.OpenCVLoader
 
 class CameraActivity : AppCompatActivity() {
 
